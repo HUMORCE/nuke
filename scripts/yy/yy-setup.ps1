0edf113a517487b1cbf80a23ddeef5c075cafda5
HUMORCE <HUMORCE@outlook.com>
2021-09-17 05:21:23 +0000

    yy-setup: Update

--- scripts/yy/yy-setup.ps1
+++ scripts/yy/yy-setup.ps1
@@ -3,44 +3,52 @@ param(
     $app_version
 )
 
+$app = "$app_dir\$app_version"
 $datas = "$env:APPDATA\duowan\yy\"
 
 $components =
-"$app_dir\$app_version\components\com.yy.cefdev2",
-"$app_dir\$app_version\components\com.yy.gameproxy",
-"$app_dir\$app_version\components\com.yy.processservice",
-"$app_dir\$app_version\components\com.yy.vip"
+"components\com.yy.cefdev2",
+"components\com.yy.gameproxy",
+"components\com.yy.processservice",
+"components\com.yy.vip"
 
 $junks =
-"$app_dir\$app_version\BugReport.exe",
-"$app_dir\$app_version\crashreport.dll",
-"$app_dir\$app_version\dwupdate.dll",
-"$app_dir\$app_version\installstat.exe",
-"$app_dir\$app_version\libcurl.dll",
-"$app_dir\$app_version\p2ppunch.dll",
-"$app_dir\$app_version\RoVee.dll",
-"$app_dir\$app_version\videotools.dll",
-"$app_dir\$app_version\widgetchannel.dll",
-"$app_dir\$app_version\yyacc.dll",
-"$app_dir\$app_version\yyaccessiblehelper.dll",
-"$app_dir\$app_version\yyadhelper.dll",
-"$app_dir\$app_version\yybrowser.exe",
-"$app_dir\$app_version\yychappplatform.dll",
-"$app_dir\$app_version\yyiecontainer.dll",
-"$app_dir\$app_version\yyp2pvideohandler.dll",
-"$app_dir\$app_version\yyplugin.dll",
-"$app_dir\$app_version\yystatistics.dll",
-"$app_dir\$app_version\yyversion.dll",
-"$app_dir\$app_version\yyvoicechanger.dll"
+"BugReport.exe",
+"crashreport.dll",
+"dwupdate.dll",
+"installstat.exe",
+"libcurl.dll",
+"p2ppunch.dll",
+"RoVee.dll",
+"videotools.dll",
+"widgetchannel.dll",
+"yyacc.dll",
+"yyaccessiblehelper.dll",
+"yyadhelper.dll",
+"yybrowser.exe",
+"yychappplatform.dll",
+"yyiecontainer.dll",
+"yyp2pvideohandler.dll",
+"yyplugin.dll",
+"yystatistics.dll",
+"yyversion.dll",
+"yyvoicechanger.dll",
+# addictions
+"preloadexternalproxy.dll",
+"udbreportsdk.dll",
+"YY.exe",
+"yypersonalfont.dll",
+"yyrun.exe",
+"yysdkcontainer.dll"
 
-foreach ($i in $junks) {
-    Remove-Item $i -Force -Recurse
+foreach ($junk in $junks) {
+    Remove-Item "$app\$junk" -Force -Recurse
 }
 
-foreach ($i in $components) {
-    Remove-Item $i -Force -Recurse
-    New-Item $i | Out-Null
-    (Get-Item $i).Attributes = "Readonly"
+foreach ($component in $components) {
+    Remove-Item "$app\$component" -Force -Recurse
+    New-Item "$app\$component" | Out-Null
+    (Get-Item "$app\$component").Attributes = "Readonly"
 }
 
 if (Test-Path $datas) {
