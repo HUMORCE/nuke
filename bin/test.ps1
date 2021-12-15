a96bda5ee6881e07c0bfe04f5b8d31315cac90f4
HUMORCE <HUMORCE@outlook.com>
2021-12-15 19:29:39 +0000

    chore(scripts): sync upstream

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,4 @@
-<#
-.SYNOPSIS
-    Execute Pester tests in repository root directory.
-#>
+#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
 
-$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
-
-exit $result.FailedCount
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+Invoke-Pester "$psscriptroot/.."
